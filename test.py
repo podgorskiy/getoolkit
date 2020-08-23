ab55457762cc20e89a593fa5e134a0f65f9fc619
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-08-23 12:02:23 -0400

    Updated test.py

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ class App(anntoolkit.App):
                 self.annotation = pickle.load(f)
 
         self.moving = None
+        self.nearest = None
         self.load_next()
 
         print("Data size: %d" % len(self.annotation.items()))
@@ -41,7 +42,6 @@ class App(anntoolkit.App):
         self.iter = (self.iter + len(self.paths)) % len(self.paths)
         im = imageio.imread(os.path.join(self.path, self.paths[self.iter]))
         self.set_image(im)
-
 
     def load_next_not_annotated(self):
         while True:
@@ -75,15 +75,18 @@ class App(anntoolkit.App):
 
     def on_update(self):
         k = self.paths[self.iter]
-        self.text(k, 10, 10)
-        self.text('\033[32mGreen \033[1;31mBold red \033[22mNormal red \033[1;34;47m Bold blue on white \033[0mReset', 10, 60)
+        self.text(k, 10, 30)
+        self.text('\033[32mGreen \033[1;31mBold red \033[22mNormal red \033[1;34;47m Bold blue on white \033[0mReset', 10, 70)
+        self.text('Window size: %dx%d' % (self.width, self.height), 10, 100)
+        self.text('Scale %f' % self.scale, 10, 130)
         if k in self.annotation:
             self.text("Points count %d" % len(self.annotation[k]), 10, 50)
             for i, p in enumerate(self.annotation[k]):
+                r = 8 if i == self.nearest else 5
                 if i == self.moving:
-                    self.point(*p, (0, 255, 0, 250))
+                    self.point(*p, (0, 255, 0, 250), radius=r)
                 else:
-                    self.point(*p, (255, 0, 0, 250))
+                    self.point(*p, (255, 0, 0, 250), radius=r)
 
             n = 2
             boxes = [self.annotation[k][i:i + n] for i in range(0, len(self.annotation[k]), n)]
@@ -92,19 +95,25 @@ class App(anntoolkit.App):
                     self.text_loc("I'm label", *box[0], (0, 10, 0, 250), (150, 255, 150, 150))
                     self.box(box, (0, 255, 0, 250), (100, 255, 100, 50))
 
+    def get_nearset(self, lx, ly, threshold=6):
+        k = self.paths[self.iter]
+        if k in self.annotation:
+            points = np.asarray(self.annotation[k])
+            if len(points) > 0:
+                point = np.asarray([[lx, ly]])
+                d = points - point
+                d = np.linalg.norm(d, axis=1)
+                i = np.argmin(d)
+                distance_to_nearset_point = d[i]
+                distance_in_window_space = self.scale * distance_to_nearset_point
+                if distance_in_window_space < threshold:
+                    return i
+        return None
+
     def on_mouse_button(self, down, x, y, lx, ly):
         k = self.paths[self.iter]
-        if down:
-            if k in self.annotation:
-                points = np.asarray(self.annotation[k])
-                if len(points) > 0:
-                    point = np.asarray([[lx, ly]])
-                    d = points - point
-                    d = np.linalg.norm(d, axis=1)
-                    i = np.argmin(d)
-                    if d[i] < 6:
-                        self.moving = i
-                        print(i, d[i])
+        if down and self.nearest is not None:
+            self.moving = self.nearest
 
         if not down:
             if k not in self.annotation:
@@ -118,6 +127,7 @@ class App(anntoolkit.App):
                 pickle.dump(self.annotation, f)
 
     def on_mouse_position(self, x, y, lx, ly):
+        self.nearest = self.get_nearset(lx, ly)
         if self.moving is not None:
             k = self.paths[self.iter]
             self.annotation[k][self.moving] = (lx, ly)
