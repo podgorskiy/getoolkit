7e2ab20593549ed845589575b6ad79b03e995ddd
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-08-24 08:27:58 -0400

    Optimized docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,9 @@
 # Script to build wheels for manylinux. This script runs inside docker.
 # See build_maylinux_wheels.sh
 
-git clone https://github.com/podgorskiy/ImageAnnotationToolkit.git
-cd ImageAnnotationToolkit
-git submodule update --init
+git clone --depth 1 https://github.com/podgorskiy/anntoolkit.git
+cd anntoolkit
+git submodule update --init  --depth 1
 cd libs/glfw
 git pull origin master
 cd ../..
@@ -13,10 +13,20 @@ yum install -y libX11-devel libXcursor-devel libXrandr-devel libXinerama-devel m
 for PYBIN in /opt/python/*/bin; do
     #"${PYBIN}/pip" install -r /io/dev-requirements.txt
     #"${PYBIN}/pip" wheel /io/ -w wheelhouse/
-    "${PYBIN}/python" setup.py bdist_wheel -d /io/wheelhouse/
+    "${PYBIN}/python" setup.py bdist_wheel -d /io/wheelhouse/ &
 done
+
+wait
+
+echo "All built"
+
+echo "Audit..."
 
 # Bundle external shared libraries into the wheels
 for whl in /io/wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/ &
 done
+
+wait
+
+echo "All done"
